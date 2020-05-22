83138604951aa68a1c22f3fc151dd5445820b784
Abdullah M. Alasmari <32742632+AbdullahMAlasmari@users.noreply.github.com>
2020-05-23 02:51:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Fall2018@localhost:5432/fyyur'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Passowrd@localhost:5432/fyyur'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
